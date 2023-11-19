c8868500ba8f48444984f156806023789f3cd5aa
voidsheep <mintk4t@gmail.com>
2023-11-19 00:41:09 -0800

    commiting this rltk version before i absolutely break it by switching to wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
-fn main() {
-    println!("Hello, world!");
+//! Rustic Rogues
+//!
+//! Jadyn Harris 2023
+
+//Source: Roguelike Tutorial - https://bfnightly.bracketproductions.com/chapter_1.html
+
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+//Source: Roguelike Tutorial - https://bfnightly.bracketproductions.com/chapter_2.html
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
 }
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+struct State {
+    ecs: World
+}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+//Source: Roguelike Tutorial - https://bfnightly.bracketproductions.com/chapter_1.html
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let mut gs = State {
+        ecs: World::new()
+    };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+    .create_entity()
+    .with(Position { x: 40, y: 25 })
+    .with(Renderable {
+        glyph: rltk::to_cp437('@'),
+        fg: RGB::named(rltk::YELLOW),
+        bg: RGB::named(rltk::BLACK),
+    })
+    .build();
+
+    for i in 0..10 {
+        gs.ecs
+        .create_entity()
+        .with(Position { x: i * 7, y: 20 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('☺'),
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    }
+    rltk::main_loop(context, gs)
+}
+
+
+
